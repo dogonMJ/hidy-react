374a803bb12e64b9045a4ecd72a5ef9e8edfe2a6
dogonMJ <b99208035@ntu.edu.tw>
2022-01-06 15:50:04 +0800

    init

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import TextInput from './TextInput'
+
+class App extends React.Component {
+  render() {
+    return <TextInput initText='put' />
+  }
+}
+
+//輸出App元件
+export default App
---
+++ src/components/TextShow.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+const TextShow = (props: { text: string }) => (
+  <h1>{props.text}</h1>
+)
+
+// 加入props的資料類型驗証
+TextShow.propTypes = {
+  text: PropTypes.string.isRequired
+}
+
+// 匯出TextShow模組
+export default TextShow
