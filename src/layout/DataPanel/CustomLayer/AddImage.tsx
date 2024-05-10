544911bb4b666200c24b55dd3bf2cccb8dbc7332
dogonMJ <b99208035@ntu.edu.tw>
2024-05-10 13:34:34 +0800

    add multiple images, proxy image

--- src/layout/DataPanel/CustomLayer/AddImage.tsx
+++ src/layout/DataPanel/CustomLayer/AddImage.tsx
@@ -1,6 +1,6 @@
 import { Box, Button, TextField, Grid, InputLabel, Stack, SxProps } from "@mui/material"
-import { ChangeEventHandler, Fragment, ReactNode, useRef, useState } from "react"
-import { ImageOverlay, useMap } from "react-leaflet"
+import { ChangeEventHandler, FocusEventHandler, Fragment, KeyboardEventHandler, ReactNode, useRef, useState } from "react"
+import { ImageOverlay } from "react-leaflet"
 import { useAlert } from "hooks/useAlert"
 import { AlertSlide } from "components/AlertSlide/AlertSlide"
 import ClearRoundedIcon from '@mui/icons-material/ClearRounded';
@@ -17,11 +17,13 @@ interface SWNE {
 interface AddImageCustomTextField {
   label: ReactNode | string
   value: any
-  onChange: ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement> | undefined
+  onChange?: ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement> | undefined
+  onBlur?: FocusEventHandler<HTMLTextAreaElement | HTMLInputElement> | undefined
+  onKeyDown?: KeyboardEventHandler<HTMLDivElement> | undefined
   sx?: SxProps
 }
 
-const CustomTextField = ({ label, value, onChange, sx }: AddImageCustomTextField) => {
+const CustomTextField = ({ label, value, onChange, onBlur, onKeyDown, sx }: AddImageCustomTextField) => {
   return (
     <TextField
       size="small"
@@ -30,23 +32,47 @@ const CustomTextField = ({ label, value, onChange, sx }: AddImageCustomTextField
       sx={{ width: 75, ...sx }}
       value={value === null ? '' : value}
       onChange={onChange}
+      onBlur={onBlur}
+      onKeyDown={onKeyDown}
     />
   );
 };
 
 export const AddImage = () => {
   const ref = useRef<any>()
-  const map = useMap()
   const dispatch = useAppDispatch()
   const { t } = useTranslation()
   const { openAlert, setOpenAlert, alertMessage, setMessage } = useAlert()
-  const url = useAppSelector(state => state.addImage.url)
   const opacity = useAppSelector(state => state.addImage.opacity)
   const bounds = useAppSelector(state => state.addImage.bbox)
-  const [swne, setSwne] = useState<SWNE>(bounds ? { south: bounds[0][0], west: bounds[0][1], north: bounds[1][0], east: bounds[1][1] } : { south: null, west: null, north: null, east: null })
-  //L.latLngBounds([20.5, 118], [26.5, 124]) https://cwaopendata.s3.ap-northeast-1.amazonaws.com/Observation/O-A0058-003.png
+  const layerName = useAppSelector(state => state.addImage.layerName)
+  const layerList = useAppSelector(state => state.addImage.layerList)
+  const externalURL = useAppSelector(state => state.addImage.url)
+  const [inputURL, setInputURL] = useState(externalURL)
+  const [inputLayerName, setInputLayerName] = useState(layerName)
+  const [swne, setSwne] = useState<SWNE>(
+    bounds ?
+      { south: bounds[0][0], west: bounds[0][1], north: bounds[1][0], east: bounds[1][1] } :
+      { south: null, west: null, north: null, east: null }
+  )
+
+  const url = `https://service.oc.ntu.edu.tw/data/proxy-image?url=${externalURL}`
+  let layeri = layerList ? layerList.length + 1 : 1
 
   const confirm = () => {
+    if (layerList && layerList.some(layer => layer.name === inputLayerName)) {
+      setMessage(t('CustomLayer.alert.dupName'))
+      layeri += 1
+      setInputLayerName(`layer${layeri}`)
+    } else {
+      dispatch(addImageSlice.actions.setLayerList([{ name: inputLayerName, url, bounds }, ...layerList]))
+    }
+  }
+
+  const handleURLBlur = () => {
+    dispatch(addImageSlice.actions.setUrl(inputURL))
+  }
+  const handleCoorBlur = () => {
     const valid = Object.values(swne).every((value) => value !== null)
     if (valid) {
       dispatch(addImageSlice.actions.setBbox([[swne.south!, swne.west!], [swne.north!, swne.east!]]))
@@ -54,12 +80,11 @@ export const AddImage = () => {
       setMessage(t('alert.noCoords'))
     }
   }
-
   const clear = () => {
     dispatch(addImageSlice.actions.setUrl(''))
     dispatch(addImageSlice.actions.setBbox(undefined))
+    setInputURL('')
     setSwne({ south: null, west: null, north: null, east: null })
-    ref.current && map.removeLayer(ref.current)
   }
 
   const handleOpacity = (event: Event, newValue: number | number[]) => {
@@ -72,13 +97,26 @@ export const AddImage = () => {
   return (
     <>
       <Box>
+        <InputLabel>{t('CustomLayer.layerName')}</InputLabel>
+        <TextField
+          size="small"
+          value={inputLayerName}
+          onChange={(e) => setInputLayerName(e.target.value)}
+          onBlur={() => dispatch(addImageSlice.actions.setLayerName(inputLayerName))}
+        />
         <InputLabel>{t('CustomLayer.enterUrl')}</InputLabel>
         <TextField
           size="small"
-          value={url}
+          value={inputURL}
           sx={{ width: 328 }}
           onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-            dispatch(addImageSlice.actions.setUrl(event.target.value))
+            setInputURL(event.target.value)
+          }}
+          onBlur={handleURLBlur}
+          onKeyDown={(ev) => {
+            if (ev.key.toLowerCase() === 'enter') {
+              handleURLBlur()
+            }
           }}
         />
         <InputLabel sx={{ mt: 1, mb: 1 }}>{t('CustomLayer.enterCoords')}</InputLabel>
@@ -97,6 +135,12 @@ export const AddImage = () => {
                         [dir]: Number(event.target.value),
                       }));
                     }}
+                    onBlur={handleCoorBlur}
+                    onKeyDown={(ev) => {
+                      if (ev.key.toLowerCase() === 'enter') {
+                        handleCoorBlur()
+                      }
+                    }}
                   />
                 </Grid >
                 <Grid item xs={4}></Grid>
@@ -105,13 +149,32 @@ export const AddImage = () => {
           }
         </Grid>
         <Stack direction='row' spacing={1} justifyContent="flex-end" sx={{ mt: 2, mb: 2, pr: 3 }}>
-          <Button size="small" variant='contained' color='error' startIcon={<ClearRoundedIcon />} onClick={clear}>{t('CustomLayer.clearLayer')}</Button>
+          <Button size="small" variant='contained' color='error' startIcon={<ClearRoundedIcon />} onClick={clear}>{t('CustomLayer.clearData')}</Button>
           <Button size="small" variant='contained' color='primary' startIcon={<CheckRoundedIcon />} onClick={confirm}>{t('CustomLayer.addLayer')}</Button>
         </Stack>
         <OpacitySlider opacity={opacity} onChange={handleOpacity} />
+        {layerList &&
+          layerList.map((layer, index) => (
+            <Button
+              key={layer.name}
+              onClick={() => dispatch(addImageSlice.actions.setLayerList(layerList.filter((_, i) => i !== index)))}
+              size="small"
+              variant='contained'
+              color='error'
+              startIcon={<ClearRoundedIcon />}
+              sx={{ marginRight: '3px' }}
+            >
+              {layer.name}
+            </Button>
+          ))}
       </Box >
-      {url && bounds && <ImageOverlay ref={ref} url={url} bounds={bounds} crossOrigin='anonymous' zIndex={600} opacity={opacity / 100} eventHandlers={eventHandlers}
-      />
+      {url && bounds &&
+        <ImageOverlay ref={ref} url={url} bounds={bounds} crossOrigin='anonymous' zIndex={405} opacity={opacity / 100} eventHandlers={eventHandlers} />
+      }
+      {layerList && layerList.length > 0 &&
+        layerList.map(layer =>
+          <ImageOverlay key={layer.name} url={layer.url} bounds={layer.bounds} crossOrigin='anonymous' opacity={opacity / 100} eventHandlers={eventHandlers} />
+        )
       }
       <AlertSlide open={openAlert} setOpen={setOpenAlert} severity='error' timeout={3000} > {alertMessage} </AlertSlide>
     </>
--- src/store/slice/addImageSlice.tsx
+++ src/store/slice/addImageSlice.tsx
@@ -2,15 +2,17 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { initNumber, initString, readUrlQuery } from "Utils/UtilsStates";
 import { LatLngTuple } from "leaflet";
 
-
 const query: any = readUrlQuery('addImage')
+
 interface AddImageStates {
   url: string
   bbox: LatLngTuple[] | undefined
   opacity: number
+  layerList: any[]
+  layerName: string
 }
 
-const initBbox = (queryObject: any, par: string) => {
+const initList = (queryObject: any, par: string) => {
   if (queryObject && queryObject[par]) {
     return JSON.parse(queryObject[par])
   } else {
@@ -22,8 +24,10 @@ export const addImageSlice = createSlice({
   name: "addImage",
   initialState: {
     url: initString(query, 'url', ''),
-    bbox: initBbox(query, 'bbox'),
+    bbox: initList(query, 'bbox'),
     opacity: initNumber(query, 'opacity', 100),
+    layerList: initList(query, 'layerList'),
+    layerName: initString(query, 'layerName', 'layer1')
   } as AddImageStates,
   reducers: {
     setUrl: (state, action: PayloadAction<string>) => {
@@ -34,6 +38,12 @@ export const addImageSlice = createSlice({
     },
     setOpacity: (state, action: PayloadAction<number>) => {
       state.opacity = action.payload
+    },
+    setLayerList: (state, action: PayloadAction<any>) => {
+      state.layerList = action.payload
+    },
+    setLayerName: (state, action: PayloadAction<string>) => {
+      state.layerName = action.payload
     },
   }
 });
